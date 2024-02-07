ea686f98c227e451f82f952a606836457c8cd83b
martin5169 <martincommisso@hotmail.com>
2024-02-07 14:13:40 -0300

    se agrega selector de puntos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import "./App.css";
-import confetti from "canvas-confetti"
+import confetti from "canvas-confetti";
 
-export const TURNS = {
+const TURNS = {
   X: "❌",
   O: "⚪",
 };
@@ -35,15 +35,34 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
-  const [winner, setWinner] = useState(null);
+  const [gameWinner, setGameWinner] = useState(null);
+  const [pointWinner,setPointWinner] = useState(null)
+  const [pointSelection, setPointSelection] = useState(0);
+  const [pointsX, setPointsX] = useState(0);
+  const [pointsO, setPointsO] = useState(0);
 
-  const reiniciar = () => {
+  const reset = () => {
     setBoard(Array(9).fill(null));
-    setWinner(null);
-    setTurn(TURNS.X)
+    setPointWinner(null);
+    setGameWinner(null)
+    setTurn(TURNS.X);
   };
 
-  const checkWinner = (boardToCheck) => {
+  const continueGame = () => {
+    setPointWinner(false);
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    const newWinner = checkGameWinner();
+    
+    if (newWinner) {
+      confetti();
+      setGameWinner(newWinner);
+    } else if (checkEndGame(board)) {
+      setWinner(false);
+    }
+  };
+
+  const checkPointWinner = (boardToCheck) => {
     for (const combo of WINNER_COMBOS) {
       const [a, b, c] = combo;
       if (
@@ -51,7 +70,16 @@ function App() {
         boardToCheck[a] === boardToCheck[b] &&
         boardToCheck[a] === boardToCheck[c]
       ) {
-        console.log(boardToCheck)
+
+        setPointWinner(boardToCheck[a])
+        let point = boardToCheck[a];
+        if (point === TURNS.X) {
+          setPointsX(prevPointsX => prevPointsX + 1);
+          console.log(pointsX)
+        } else {
+          setPointsO(prevPointsO => prevPointsO + 1);
+        }
+        
         return boardToCheck[a];
       }
     }
@@ -59,13 +87,27 @@ function App() {
     return null;
   };
 
-  const checkEndGame = (newBoard)=>{
-      return newBoard.every((square)=>square!=null)
+  const checkGameWinner = ()=>{
+    if (pointsO===pointSelection){
+      return  TURNS.O;
+    } else if (pointsX===pointSelection){   
+      return TURNS.X;
+    }
+    return null;
   }
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square != null);
+  };
+
+  const handleSelection = (points) => {
+    setPointSelection(points);
+    console.log(points)
+  };
 
   const updateBoard = (index) => {
     //esto es para que no me deje escribir o cambiar la opcion ya tomada
-    if (board[index] || winner) return;
+    if (board[index] || gameWinner) return;
     //aca actualizamos el tablero, en base a lo que ingreso el usuario del turno
     const newBoard = [...board];
     newBoard[index] = turn;
@@ -73,50 +115,97 @@ function App() {
     //aca marcamos el turno correspondiente, si esta en X pasa a O y viceversa
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    checkPointWinner(newBoard);
     //revisamos si hay un ganador
-    const newWinner = checkWinner(newBoard);
-    if (newWinner) {
-      confetti()
-      setWinner(newWinner);
-    }else if (checkEndGame(newBoard)){
-      setWinner(false)
-    }
+  
+    
   };
 
   return (
     <main className="board">
       <div className="logo">
-      <h1>Tic Tac Toe</h1>
+        <h1>Tic Tac Toe</h1>
       </div>
+
       <section className="turn">
-        {turn === TURNS.X ? <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>:""}
-        {turn === TURNS.O ? <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>:""}
+        {turn === TURNS.X ? (
+          <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        ) : (
+          ""
+        )}
+        {turn === TURNS.O ? (
+          <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+        ) : (
+          ""
+        )}
       </section>
-      
+
       <section className="game">
         {board.map((square, index) => {
           return (
             <Square key={index} index={index} updateBoard={updateBoard}>
-              {" "}
               {square}
             </Square>
           );
         })}
       </section>
-      
-      <button onClick={reiniciar}>REINICIAR</button>
 
-      {winner != null && (
+      <button onClick={reset}>REINICIAR</button>
+
+      {gameWinner !== null && (
         <section className="winner">
           <div className="text">
-            <h2>
-              {winner === false ? "Empate" : "Ganó"}
-            </h2>
+            <h2>{gameWinner === false ? "Empate" : "Ganó"}</h2>
             <header className="win">
-              {winner && <Square>{winner}</Square>}
+              {gameWinner && <Square>{gameWinner}</Square>}
             </header>
             <footer>
-              <button onClick={reiniciar}>Reiniciar Juego</button>
+              <button onClick={reset}>Reiniciar Juego</button>
+            </footer>
+          </div>
+        </section>
+      )}
+
+      {pointWinner && (
+        <section className="winner">
+          <div className="text">
+            <h2>{pointWinner != null ? "Punto" : ""}</h2>
+            <header className="win">
+              {pointWinner && <Square>{pointWinner}</Square>}
+            </header>
+            <footer>
+              <button onClick={continueGame}>Continuar</button>
+            </footer>
+          </div>
+        </section>
+      )}
+
+      {pointSelection === 0 && (
+        <section className="winner">
+          <div className="text">
+            <h3>Seleccioná la cantidad de puntos a jugar</h3>
+            <footer>
+              <button
+                onClick={(event) => {
+                  handleSelection(parseInt(event.target.textContent));
+                }}
+              >
+                3
+              </button>
+              <button
+                onClick={(event) => {
+                  handleSelection(event.target.textContent);
+                }}
+              >
+                5
+              </button>
+              <button
+                onClick={(event) => {
+                  handleSelection(event.target.textContent);
+                }}
+              >
+                7
+              </button>
             </footer>
           </div>
         </section>
